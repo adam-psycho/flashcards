a2b1a976fbd5e68051f9412912b85e4fe2e94885
Adam <adamvoliva+1@gmail.com>
2020-09-20 00:18:55 -0700

    Add ability to select/deselect words.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent {
   	this.currentCardIndex = this.getRandomCardIndex();
   }
 
-  public data = hanzi;
+  public data = JSON.parse(JSON.stringify(hanzi));
+  public selectedData = JSON.parse(JSON.stringify(hanzi));
   public keys: string[] = hanzi.keys;
   public sides = {
   	front: ['hanzi'],
@@ -22,25 +23,28 @@ export class AppComponent {
   };
   public cardFlipped: boolean = false;
   private previousKeys: number[] = [];
+  public onFlashcards = true;
 
   public currentCardIndex: number;
 
   private getRandomCardIndex(): number {
-  	const cardCount = this.data.cards.length;
+  	const cardCount = this.selectedData.cards.length;
   	return Math.floor(Math.random() * cardCount);
   }
 
   public nextCard(): void {
-  	let newCardIndex = this.getRandomCardIndex();
-  	if (this.previousKeys.length === (this.data.cards.length - 1)) {
-  		this.previousKeys = [];
+  	if (this.selectedData.cards.length > 1) {
+	  	let newCardIndex = this.getRandomCardIndex();
+	  	if (this.previousKeys.length >= (this.selectedData.cards.length - 1)) {
+	  		this.previousKeys = [];
+	  	}
+	  	while (this.previousKeys.indexOf(newCardIndex) !== -1 || newCardIndex === this.currentCardIndex) {
+	  		newCardIndex = this.getRandomCardIndex();
+	  	}
+	  	this.previousKeys.push(this.currentCardIndex);
+	  	console.log(this.previousKeys);
+	  	this.currentCardIndex = newCardIndex;
   	}
-  	while (this.previousKeys.indexOf(newCardIndex) !== -1 || newCardIndex === this.currentCardIndex) {
-  		newCardIndex = this.getRandomCardIndex();
-  	}
-  	this.previousKeys.push(this.currentCardIndex);
-  	console.log(this.previousKeys);
-  	this.currentCardIndex = newCardIndex;
   }
 
   public setKeys(side: string, key: string): void {
@@ -57,5 +61,30 @@ export class AppComponent {
 
   public flipCard(): void {
   	this.cardFlipped = !this.cardFlipped;
+  }
+
+  public isCardSelected(card): boolean {
+  	let key = this.keys[0];
+  	return this.selectedData.cards.filter(x => x[key] === card[key]).length > 0;
+  }
+
+  public selectCard(card, checked: boolean): void {
+  	if (!checked) {
+  		let key = this.keys[0];
+  		let cards =this.selectedData.cards.filter(x => x[key] !== card[key]);
+  		this.selectedData.cards = cards;
+  	} else {
+  		this.selectedData.cards.push(card);
+  	}
+  	this.currentCardIndex = this.getRandomCardIndex();
+  }
+
+  public selectAll(checked: boolean): void {
+  	if (checked) {
+  		this.selectedData.cards = JSON.parse(JSON.stringify(this.data.cards));
+  	} else {
+  		this.selectedData.cards = [];
+  	}
+  	this.currentCardIndex = this.getRandomCardIndex();
   }
 }
--- src/app/models/hanzi.ts
+++ src/app/models/hanzi.ts
@@ -429,5 +429,171 @@ export const hanzi = {
 			pinyin: 'shā',
 			english: 'Shark'
 		},
+		/* ep 3&4 */
+		{
+			hanzi: '母',
+			pinyin: 'fùmǔ ',
+			english: 'parents'
+		},
+		{
+			hanzi: '爸爸',
+			pinyin: 'bàba',
+			english: 'father'
+		},
+		{
+			hanzi: '妈妈',
+			pinyin: 'māma ',
+			english: 'parents'
+		},
+		{
+			hanzi: '好父母',
+			pinyin: 'hǎo fùmǔ',
+			english: 'Good parents'
+		},
+		{
+			hanzi: '我',
+			pinyin: 'wǒ',
+			english: 'I ; me'
+		},
+		{
+			hanzi: '叫',
+			pinyin: 'jiào',
+			english: 'call; to be called'
+		},
+		{
+			hanzi: '我叫小明',
+			pinyin: 'Wǒ jiào Xiǎo Míng',
+			english: 'I am Xiǎo Míng'
+		},
+		{
+			hanzi: '小明，你好',
+			pinyin: 'Xiǎo Míng ，nǐ hǎo',
+			english: 'Hello Xiao Ming'
+		},
+		{
+			hanzi: '你好，我叫小明',
+			pinyin: 'Nǐ hǎo ，wǒ jiào Xiǎo Míng',
+			english: 'Hello, I am Xiao Ming'
+		},
+		{
+			hanzi: '什么',
+			pinyin: 'shénme',
+			english: 'what'
+		},
+		{
+			hanzi: '你好，我叫小明。你叫什么？',
+			pinyin: 'Nǐ jiào shénme ？',
+			english: 'What is your name?'
+		},
+		{
+			hanzi: '名字',
+			pinyin: 'míngzi',
+			english: 'name'
+		},
+		{
+			hanzi: '好名字',
+			pinyin: 'hǎo míngzi ',
+			english: 'good name'
+		},
+		{
+			hanzi: '什么名字？',
+			pinyin: 'shénme míngzi ',
+			english: 'What name?'
+		},
+		{
+			hanzi: '你叫什么名字？',
+			pinyin: 'Nǐ jiào shénme míngzi ？',
+			english: 'What\'s your name?'
+		},
+		{
+			hanzi: '姓',
+			pinyin: ' xìng',
+			english: 'surname; last name'
+		},
+		{
+			hanzi: '你姓什么？',
+			pinyin: 'Nǐ xìng shénme ？',
+			english: 'What\'s your last name?'
+		},
+		{
+			hanzi: '王',
+			pinyin: 'wáng',
+			english: 'a surname; king'
+		},
+		{
+			hanzi: '噢',
+			pinyin: 'o',
+			english: 'Oh'
+		},
+		{
+			hanzi: '你好，我叫麦克',
+			pinyin: 'Nǐ hǎo ，wǒ jiào Màikè',
+			english: 'Hello, I am Mike'
+		},
+		{
+			hanzi: '我(的)叔叔',
+			pinyin: 'wǒ (de) shūshu ',
+			english: 'I\'m an uncle.'
+		},
+		{
+			hanzi: '叔叔好',
+			pinyin: 'Shūshu hǎo',
+			english: 'Hello uncle'
+		},
+		{
+			hanzi: '叔，早上好。',
+			pinyin: 'Shūshu ，zǎoshàng hǎo ',
+			english: 'Good morning, uncle.'
+		},
+		{
+			hanzi: '阿姨',
+			pinyin: 'āyí',
+			english: 'aunt'
+		},
+		{
+			hanzi: '阿姨，早上好。',
+			pinyin: 'Āyí ，zǎoshàng hǎo',
+			english: 'Good morning, aunt.'
+		},
+		{
+			hanzi: '谢谢阿姨',
+			pinyin: 'Xiěxie āyí',
+			english: 'Thank you aunt.'
+		},
+		{
+			hanzi: '多大',
+			pinyin: 'duō dà',
+			english: 'how old?'
+		},
+		{
+			hanzi: '你多大了？',
+			pinyin: 'Nǐ duō dà le ？',
+			english: 'How old are you?'
+		},
+		{
+			hanzi: '岁',
+			pinyin: 'suì',
+			english: 'years old'
+		},
+		{
+			hanzi: ' 我二十岁',
+			pinyin: 'Wǒ èr shí suì',
+			english: 'I\'m twenty'
+		},
+		{
+			hanzi: '你叔叔多大了？',
+			pinyin: 'Nǐ shūshu duō dà le ？',
+			english: 'How old is your uncle?'
+		},
+		{
+			hanzi: '呢',
+			pinyin: 'ne',
+			english: 'a sentence-final particle'
+		},
+		{
+			hanzi: '也',
+			pinyin: 'yě',
+			english: 'also; too'
+		},
 	]
 }
